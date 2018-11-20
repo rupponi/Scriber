30aa67be44b36625bbc49626dc23d57d07f4ca41
rupponi <rponi70@gmail.com>
2018-11-20 02:59:30 -0700

    Branch 5: Added registration activities for doctor and patient flows which will be developed in following changes.

--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/DoctorLogin.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/DoctorLogin.java
@@ -1,7 +1,10 @@
 package com.example.rohanupponi.scribeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DoctorLogin extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class DoctorLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_login);
+
+        Button doctorLoginButton = (Button) findViewById(R.id.doctor_login_button);
+        doctorLoginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // TODO: Login functionality to add later (Firebase authentication).
+            }
+        });
+
+
+        Button doctorForgotPassButton = (Button) findViewById(R.id.doctor_forgot_password_button);
+        doctorForgotPassButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // TODO: Forgot password functionality to add later (Generate form for sending email to inputted user email).
+            }
+        });
+
+
+        Button doctorRegisterButton = (Button) findViewById(R.id.doctor_register_button);
+        doctorRegisterButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent registerDoctor = new Intent(DoctorLogin.this, DoctorRegistration.class);
+                startActivity(registerDoctor);
+            }
+        });
     }
 }
--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PatientLogin.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PatientLogin.java
@@ -1,7 +1,10 @@
 package com.example.rohanupponi.scribeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientLogin extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class PatientLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_login);
+
+        Button patientLoginButton = (Button) findViewById(R.id.patient_login_button);
+        patientLoginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // TODO: Patient login authentication (Firebase).
+            }
+        });
+
+        Button patientForgotPassButton = (Button) findViewById(R.id.patient_forgot_password_button);
+        patientForgotPassButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // TODO: Patient forgot password (Forgot password form to send email recovery).
+            }
+        });
+
+
+        Button patientRegisterButton = (Button) findViewById(R.id.patient_register_button);
+        patientRegisterButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent registerPatient = new Intent(PatientLogin.this, PatientRegistration.class);
+                startActivity(registerPatient);
+            }
+        });
+
     }
 }
