e5cbda27a8f71fff87ca13be5853f05fa85fbd58
Rohan Upponi <rponi70@gmail.com>
2019-02-26 17:33:26 -0700

    Update: - Patient login now retrieves profile info stored in database.

--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PatientHome.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PatientHome.java
@@ -1,16 +1,26 @@
 package com.example.rohanupponi.scribeapp;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class PatientHome extends AppCompatActivity {
     public static final String TAG = "PatientHome";
 
     private FloatingActionButton patientEditProfile;
+    private TextView greetingTitle;
+    private TextView patientName, patientEmail, patientAddress, patientCity, patientState, patientZip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,49 @@ public class PatientHome extends AppCompatActivity {
                 startActivity(openProfile);
             }
         });
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String[] patientCreds = getIntent().getStringArrayExtra("patient_creds");
+
+        greetingTitle = findViewById(R.id.patient_greeting);
+        patientName = findViewById(R.id.patient_name);
+        patientEmail = findViewById(R.id.patient_email);
+        patientAddress = findViewById(R.id.patient_address);
+        patientCity = findViewById(R.id.patient_city);
+        patientState = findViewById(R.id.patient_state);
+        patientZip = findViewById(R.id.patient_zip);
+
+        db.collection("patients").document(patientCreds[0]).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    // Acquire all fields.
+                    String name = task.getResult().get("name").toString();
+                    String nameGreeting = "Welcome " + name + "!";
+                    String email = task.getResult().getId();
+                    String address = task.getResult().get("street-address").toString();
+                    String city = task.getResult().get("city").toString();
+                    String state = task.getResult().get("state").toString();
+                    String zip = task.getResult().get("zip-code").toString();
+
+                    // Set all textviews.
+                    greetingTitle.setText(nameGreeting);
+                    patientName.setText("Name: " + name);
+                    patientEmail.setText("Email: " + email);
+                    patientAddress.setText("Address: " + address);
+                    patientCity.setText("City: " + city);
+                    patientState.setText("State: " + state);
+                    patientZip.setText("ZIP: " + zip);
+                }
+                else {
+                    Toast retry = Toast.makeText(getApplicationContext(), "Sorry we could not retrieve your data at this time.", Toast.LENGTH_LONG);
+                    retry.show();
+                    Intent returnToLogin = new Intent(getApplicationContext(), LoginPage.class);
+                    startActivity(returnToLogin);
+                }
+            }
+        });
+
 
     }
 }
