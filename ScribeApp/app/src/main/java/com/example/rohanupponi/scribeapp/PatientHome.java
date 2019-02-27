d61f8b8c01ed91b7765c072621f66e283e1551ca
Vasishta PC <vsom97@hotmail.com>
2019-02-27 15:55:29 -0700

    Vasishta: Added tabs on patient home

--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PatientHome.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PatientHome.java
@@ -1,21 +1,59 @@
 package com.example.rohanupponi.scribeapp;
 
+import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
-public class PatientHome extends AppCompatActivity {
+
+public class PatientHome extends AppCompatActivity implements PersonalTab.OnFragmentInteractionListener, MedicalTab.OnFragmentInteractionListener, NotesTab.OnFragmentInteractionListener {
     public static final String TAG = "PatientHome";
 
     private FloatingActionButton patientEditProfile;
+    Button PatientLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_home);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Personal"));
+        tabLayout.addTab(tabLayout.newTab().setText("Medical"));
+        tabLayout.addTab(tabLayout.newTab().setText("Notes"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+
+
         patientEditProfile = findViewById(R.id.Patient_Edit_Profile);
         patientEditProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +63,26 @@ public class PatientHome extends AppCompatActivity {
             }
         });
 
+        PatientLogout = findViewById(R.id.logoutButton);
+        PatientLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Logout = new Intent(getApplicationContext(), LoginPage.class);
+                startActivity(Logout);
+
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
+
+
+
+
+
