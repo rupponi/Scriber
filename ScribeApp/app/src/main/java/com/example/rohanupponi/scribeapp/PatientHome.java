160f89bde8029916228e242e2017be4230c64f67
Rohan Upponi <rponi70@gmail.com>
2019-03-03 18:21:27 -0700

    Update: - Pulls from database with new view. - Floating logout and edit buttons.

--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PatientHome.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PatientHome.java
@@ -1,24 +1,70 @@
 package com.example.rohanupponi.scribeapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 public class PatientHome extends AppCompatActivity {
+
+    static DocumentSnapshot patientData;
+    private FloatingActionButton editProfileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_home);
 
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(new MainViewPagerAdapter(getSupportFragmentManager()));
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String[] patientCreds = getIntent().getStringArrayExtra("patient_creds");
 
-        TabLayout profileTabs = findViewById(R.id.profile_tabs);
-        profileTabs.setTabTextColors(ContextCompat.getColor(getApplicationContext(), R.color.unselectedTab), ContextCompat.getColor(getApplicationContext(), R.color.selectedTab));
+        db.collection("patients").document(patientCreds[0]).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    patientData = task.getResult();
 
-        profileTabs.setupWithViewPager(viewPager);
+                    ViewPager viewPager = findViewById(R.id.view_pager);
+                    viewPager.setAdapter(new MainViewPagerAdapter(getSupportFragmentManager()));
+
+                    TabLayout profileTabs = findViewById(R.id.profile_tabs);
+                    profileTabs.setTabTextColors(ContextCompat.getColor(getApplicationContext(), R.color.unselectedTab), ContextCompat.getColor(getApplicationContext(), R.color.selectedTab));
+
+                    profileTabs.setupWithViewPager(viewPager);
+                }
+                else {
+                    Toast dataAccessFailure = Toast.makeText(getApplicationContext(), "Your profile is currently unavailable.", Toast.LENGTH_LONG);
+                    dataAccessFailure.show();
+                    Intent returnToLogin = new Intent(getApplicationContext(), LoginPage.class);
+                    startActivity(returnToLogin);
+                    return;
+                }
+            }
+        });
+
+        editProfileButton = findViewById(R.id.edit_profile_button);
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent editProfile = new Intent(getApplicationContext(), PatientProfile.class);
+                startActivity(editProfile);
+            }
+        });
+
+
     }
 }
--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PersonalTab.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/PersonalTab.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import javax.annotation.Nullable;
 
@@ -14,6 +15,21 @@ public class PersonalTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View fragView = inflater.inflate(R.layout.fragment_personal_tab, container, false);
+
+        TextView patientName = fragView.findViewById(R.id.patient_name);
+        TextView patientEmail = fragView.findViewById(R.id.patient_email);
+        TextView patientStreetAddress = fragView.findViewById(R.id.patient_street_address);
+        TextView patientCity = fragView.findViewById(R.id.patient_city);
+        TextView patientState = fragView.findViewById(R.id.patient_state);
+        TextView patientZip = fragView.findViewById(R.id.patient_zip);
+
+        patientName.setText("Name: \t" + PatientHome.patientData.get("name").toString());
+        patientEmail.setText("Email: \t" + PatientHome.patientData.getId());
+        patientStreetAddress.setText("Street Address: \t" + PatientHome.patientData.get("street-address").toString());
+        patientCity.setText("City: \t" + PatientHome.patientData.get("city").toString());
+        patientState.setText("State: \t" + PatientHome.patientData.get("state").toString());
+        patientZip.setText("ZIP: \t" + PatientHome.patientData.get("zip-code").toString());
+
         return fragView;
     }
 }
