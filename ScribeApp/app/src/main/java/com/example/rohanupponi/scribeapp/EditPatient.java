c444397fa1ffa27f08b5b7ecacafbf5c72449607
Vasishta PC <vsom97@hotmail.com>
2019-03-11 20:45:26 -0700

    Update: -Added update fields to edit personal tab

--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/EditPatient.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/EditPatient.java
@@ -48,16 +48,59 @@ public class EditPatient extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                EditText Name = findViewById(R.id.NameInput);
-                EditText Email = findViewById(R.id.EmailInput);
-                EditText Date = findViewById(R.id.AgeInput);
-                EditText Phone = findViewById(R.id.ContactInput);
+                EditText Name = findViewById(R.id.ENameInput);
+                EditText Email = findViewById(R.id.EEmailInput);
+                EditText Date = findViewById(R.id.EAgeInput);
+                EditText Phone = findViewById(R.id.EPhoneInput);
+
+                EditText Gender = findViewById(R.id.EGenderInput);
+                EditText Street = findViewById(R.id.EStreetInput);
+                EditText City = findViewById(R.id.ECityInput);
+                EditText State = findViewById(R.id.EStateInput);
+                EditText Zip = findViewById(R.id.EZipInput);
+                EditText Ethnicity = findViewById(R.id.EEthnicityInput);
+                EditText Marital = findViewById(R.id.EMaritalInput);
+                EditText PInsurance = findViewById(R.id.EPrimaryInsuranceInput);
+                EditText PInsurancePolicy = findViewById(R.id.EPrimaryPolicyInput);
+                EditText PInsuranceGroup = findViewById(R.id.EPrimaryGroupInput);
+                EditText SInsurance = findViewById(R.id.ESecondaryInsuranceInput);
+                EditText SInsurancePolicy = findViewById(R.id.ESecondaryPolicyInput);
+                EditText SInsuranceGroup = findViewById(R.id.ESecondaryGroupInput);
+                EditText Employer = findViewById(R.id.EEmployerInput);
+                EditText EmployerStreet = findViewById(R.id.EEmployerStreetInput);
+                EditText EmployerCity = findViewById(R.id.EEmployerCityInput);
+                EditText EmployerState = findViewById(R.id.EEmployerStateInput);
+                EditText EmployerZip = findViewById(R.id.EEmployerZipInput);
+
+
+
+
 
 
                 String Na = Name.getText().toString().trim();
                 String Da = (Date.getText().toString().trim());
                 String Ph = (Phone.getText().toString().trim());
                 String Em = Email.getText().toString().trim();
+
+                String Ge = Gender.getText().toString().trim();
+                String St = Street.getText().toString().trim();
+                String Ci = City.getText().toString().trim();
+                String Sta= State.getText().toString().trim();
+                String Zi = Zip.getText().toString().trim();
+                String Eth = Ethnicity.getText().toString().trim();
+                String Mar= Marital.getText().toString().trim();
+                String In = PInsurance.getText().toString().trim();
+                String Policy = PInsurancePolicy.getText().toString().trim();
+                String Gr = PInsuranceGroup.getText().toString().trim();
+                String SIn = SInsurance.getText().toString().trim();
+                String SPolicy = SInsurancePolicy.getText().toString().trim();
+                String SGr = SInsuranceGroup.getText().toString().trim();
+                String Empl = Employer.getText().toString().trim();
+                String EmplStreet = EmployerStreet.getText().toString().trim();
+                String EmplCity = EmployerCity.getText().toString().trim();
+                String EmplState = EmployerState.getText().toString().trim();
+                String EmplZip = EmployerZip.getText().toString().trim();
+
 
                 prefs = PreferenceManager.getDefaultSharedPreferences(EditPatient.this);
                 SharedPreferences.Editor editor = prefs.edit();
@@ -68,6 +111,26 @@ public class EditPatient extends AppCompatActivity {
                 editor.putString("Ph",Ph);
                 editor.putString("Em", Em);
 
+                editor.putString("Ge", Ge);
+                editor.putString("St", St);
+                editor.putString("Ci", Ci);
+                editor.putString("Sta", Sta);
+                editor.putString("Zi", Zi);
+                editor.putString("Eth", Eth);
+                editor.putString("Mar", Mar);
+                editor.putString("In", In);
+                editor.putString("Policy", Policy);
+                editor.putString("Gr", Gr);
+                editor.putString("SIn", SIn);
+                editor.putString("SPolicy", SPolicy);
+                editor.putString("SGr", SGr);
+                editor.putString("Empl", Empl);
+                editor.putString("EmplStreet", EmplStreet);
+                editor.putString("EmplCity", EmplCity);
+                editor.putString("EmplState", EmplState);
+                editor.putString("EmplZip", EmplZip);
+
+
                 editor.apply();
 
 
@@ -77,7 +140,30 @@ public class EditPatient extends AppCompatActivity {
 
 
                 db.collection("patients").document(Em).update(
-                        "name", Na
+                        "name", Na,
+                        "street-address", St,
+                        "city", Ci,
+                        "state", Sta,
+                        "gender", Ge,
+                        "email", Em,
+                        "date-of-birth", Da,
+                        "primary-phone", Ph,
+                        "zip-code", Zi,
+                        "ethnicity", Eth,
+                        "marital-status", Mar,
+                        "primary-insurance", In,
+                        "primary-policy", Policy,
+                        "primary-group", Gr,
+                        "secondary-insurance", SIn,
+                        "secondary-policy", SPolicy,
+                        "secondary-group", SGr,
+                        "employer", Empl,
+                        "employer-street", EmplStreet,
+                        "employer-city", EmplCity,
+                        "employer-state", EmplState,
+                        "employer-zip", EmplZip
+
+
                 ).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/EditPersonalTab.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/EditPersonalTab.java
@@ -14,7 +14,7 @@ import javax.annotation.Nullable;
 public class EditPersonalTab extends Fragment {
     @Nullable
 
-    EditText Name, Email, City, StreetAddress, State, Zip, Phone;
+    EditText Name, Email, City, StreetAddress, State, Zip, Phone, Gender, Ethnicity, Marital, PInsurance, PInsurancePolicy, PInsuranceGroup, SInsurance, SInsurancePolicy, SInsuranceGroup, Employer, EmployerStreet, EmployerCity, EmployerState, EmployerZip;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,28 +22,68 @@ public class EditPersonalTab extends Fragment {
         View fragView = inflater.inflate(R.layout.fragment_edit_personal_tab, container, false);
 
 
-        Name = fragView.findViewById(R.id.NameInput);
+        Name = fragView.findViewById(R.id.ENameInput);
         Name.setText(PatientHome.patientData.get("name").toString());
 
-        Email = fragView.findViewById(R.id.EmailInput);
+        Email = fragView.findViewById(R.id.EEmailInput);
         Email.setText(PatientHome.patientData.getId());
 
-        City = fragView.findViewById(R.id.CityInput);
+        City = fragView.findViewById(R.id.ECityInput);
         City.setText(PatientHome.patientData.get("city").toString());
 
-        StreetAddress = fragView.findViewById(R.id.StreetInput);
+        StreetAddress = fragView.findViewById(R.id.EStreetInput);
         StreetAddress.setText(PatientHome.patientData.get("street-address").toString());
 
-        State = fragView.findViewById(R.id.StateInput);
+        State = fragView.findViewById(R.id.EStateInput);
         State.setText(PatientHome.patientData.get("state").toString());
 
-        Zip = fragView.findViewById(R.id.ZipInput);
+        Zip = fragView.findViewById(R.id.EZipInput);
         Zip.setText(PatientHome.patientData.get("zip-code").toString());
 
-        Phone = fragView.findViewById(R.id.ContactInput);
+        Phone = fragView.findViewById(R.id.EPhoneInput);
+        Phone.setText(PatientHome.patientData.get("primary-phone").toString());
 
+        Gender = fragView.findViewById(R.id.EGenderInput);
+        Gender.setText(PatientHome.patientData.get("gender").toString());
 
+        Ethnicity = fragView.findViewById(R.id.EEthnicityInput);
+        Ethnicity.setText(PatientHome.patientData.get("ethnicity").toString());
 
+        Marital = fragView.findViewById(R.id.EMaritalInput);
+        Marital.setText(PatientHome.patientData.get("marital-status").toString());
+
+        PInsurance = fragView.findViewById(R.id.EPrimaryInsuranceInput);
+        PInsurance.setText(PatientHome.patientData.get("primary-insurance").toString());
+
+        PInsurancePolicy = fragView.findViewById(R.id.EPrimaryPolicyInput);
+        PInsurancePolicy.setText(PatientHome.patientData.get("primary-policy").toString());
+
+        PInsuranceGroup = fragView.findViewById(R.id.EPrimaryGroupInput);
+        PInsuranceGroup.setText(PatientHome.patientData.get("primary-group").toString());
+
+        SInsurance = fragView.findViewById(R.id.ESecondaryInsuranceInput);
+        SInsurance.setText(PatientHome.patientData.get("secondary-insurance").toString());
+
+        SInsurancePolicy = fragView.findViewById(R.id.ESecondaryPolicyInput);
+        SInsurancePolicy.setText(PatientHome.patientData.get("secondary-policy").toString());
+
+        SInsuranceGroup = fragView.findViewById(R.id.ESecondaryGroupInput);
+        SInsuranceGroup.setText(PatientHome.patientData.get("secondary-group").toString());
+
+        Employer = fragView.findViewById(R.id.EEmployerInput);
+        Employer.setText(PatientHome.patientData.get("employer").toString());
+
+        EmployerStreet = fragView.findViewById(R.id.EEmployerStreetInput);
+        EmployerStreet.setText(PatientHome.patientData.get("employer-street").toString());
+
+        EmployerCity = fragView.findViewById(R.id.EEmployerCityInput);
+        EmployerCity.setText(PatientHome.patientData.get("employer-city").toString());
+
+        EmployerState = fragView.findViewById(R.id.EEmployerStateInput);
+        EmployerState.setText(PatientHome.patientData.get("employer-state").toString());
+
+        EmployerZip = fragView.findViewById(R.id.EEmployerZipInput);
+        EmployerZip.setText(PatientHome.patientData.get("employer-zip").toString());
 
         return fragView;
 
