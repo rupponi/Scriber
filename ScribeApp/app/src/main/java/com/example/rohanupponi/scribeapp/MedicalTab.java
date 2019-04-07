3cbec4193bbe7a4202de89d59fcadf8b4b65d49f
Rohan Upponi <rponi70@gmail.com>
2019-04-07 16:17:45 -0700

    Update: - Updated UI layout of medical tab.

--- ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/MedicalTab.java
+++ ScribeApp/app/src/main/java/com/example/rohanupponi/scribeapp/MedicalTab.java
@@ -34,18 +34,18 @@ public class MedicalTab extends Fragment {
         surgicalHistory = fragView.findViewById(R.id.SurgicalHistoryInput);
         conditions = fragView.findViewById(R.id.ConditionsInput);
 
-        primaryEmergency.setText("Emergency Contact: \t" + PatientHome.patientData.get("primary-em-contact").toString());
-        primaryEmergencyPhone.setText("Emergency Phone: \t" + PatientHome.patientData.get("primary-em-phone").toString());
-        secondaryEmergency.setText("Secondary Contact: \t" + PatientHome.patientData.get("secondary-em-contact").toString());
-        secondaryEmergencyPhone.setText("Secondary Phone: \t" + PatientHome.patientData.get("secondary-em-phone").toString());
-        bloodType.setText("Blood Type: \t" + PatientHome.patientData.get("blood-type").toString());
-        prescriptions.setText("Prescriptions/Dosages: \t" + PatientHome.patientData.get("prescription-dosage").toString());
-        vaccinations.setText("Vaccinations: \t" + PatientHome.patientData.get("vaccinations").toString());
-        lifestyle.setText("Lifestyle: \t" + PatientHome.patientData.get("lifestyle").toString());
-        allergies.setText("Allergies: \t" + PatientHome.patientData.get("allergies").toString());
-        familyHistory.setText("Family Medical History: \t" + PatientHome.patientData.get("family-history").toString());
-        surgicalHistory.setText("Surgical History: \t" + PatientHome.patientData.get("surgical-history").toString());
-        conditions.setText("Conditions: \t" + PatientHome.patientData.get("conditions").toString());
+        primaryEmergency.setText(PatientHome.patientData.get("primary-em-contact").toString());
+        primaryEmergencyPhone.setText(PatientHome.patientData.get("primary-em-phone").toString());
+        secondaryEmergency.setText(PatientHome.patientData.get("secondary-em-contact").toString());
+        secondaryEmergencyPhone.setText(PatientHome.patientData.get("secondary-em-phone").toString());
+        bloodType.setText(PatientHome.patientData.get("blood-type").toString());
+        prescriptions.setText(PatientHome.patientData.get("prescription-dosage").toString());
+        vaccinations.setText(PatientHome.patientData.get("vaccinations").toString());
+        lifestyle.setText(PatientHome.patientData.get("lifestyle").toString());
+        allergies.setText(PatientHome.patientData.get("allergies").toString());
+        familyHistory.setText(PatientHome.patientData.get("family-history").toString());
+        surgicalHistory.setText(PatientHome.patientData.get("surgical-history").toString());
+        conditions.setText(PatientHome.patientData.get("conditions").toString());
 
         return fragView;
     }
